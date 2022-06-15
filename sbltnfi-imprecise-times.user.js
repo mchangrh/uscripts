ee427d4b311b57d8a2fd3f0c278c6abfed8130ec
Michael C <michael@mchang.name>
2022-06-15 16:09:31 -0400

    add imprecise-times

---
+++ sbltnfi-imprecise-times.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         sb.ltn.fi imprecise times
+// @namespace    mchang.name
+// @version      1.0.0
+// @description  Make all times on sb.ltn.fi imprecise
+// @author       mchangrh
+// @match        https://sb.ltn.fi/*
+// @icon         https://sb.ltn.fi/static/browser/logo.png
+// @grant        none
+// @updateURL    https://gist.github.com/mchangrh/9507604353e37b6abc2f7f6b3c6e1338/raw/sbltnfi-imprecise-times.user.js
+// @downloadURL  https://gist.github.com/mchangrh/9507604353e37b6abc2f7f6b3c6e1338/raw/sbltnfi-imprecise-times.user.js
+// ==/UserScript==
+ 
+// replace consequtive zeros at the end with nothing
+const reduceTime = time => time.replace(/(?<=\.\d+)0+$/g, '');
+
+function findTimes() {
+  console.log('start reduceTimes')
+  const table = document.querySelector("table");
+  const headers = [...table.querySelectorAll("thead th")].map((item) =>
+    item.textContent.trim()
+  );
+  const startIndex = headers.indexOf("Start");
+  const endIndex = headers.indexOf("End");
+  const lengthIndex = headers.indexOf("Length")
+  if (startIndex === -1) return;
+  table.querySelectorAll("tbody tr").forEach((row) => {
+    for (const index of [startIndex, endIndex, lengthIndex]) {
+      console.log(index)
+      const cell = row.children[index];
+      console.log(cell)
+      const oldValue = cell.innerText
+      console.log(oldValue)
+      cell.innerText = reduceTime(oldValue);
+    }
+  });
+}
+
+(function () {
+  "use strict";
+  findTimes();
+  document.addEventListener("newSegments", (event) => findTimes());
+})();
