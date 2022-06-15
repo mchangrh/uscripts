70ac7ecd9b82479d623420c1c5d81b20d6e74adf
Michael C <michael@mchang.name>
2022-06-15 18:58:16 -0400

    add catch for clickable starttimes

--- sbltnfi-imprecise-times.user.js
+++ sbltnfi-imprecise-times.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sb.ltn.fi imprecise times
 // @namespace    mchang.name
-// @version      1.0.1
+// @version      1.1.0
 // @description  Make all times on sb.ltn.fi imprecise
 // @author       mchangrh
 // @match        https://sb.ltn.fi/*
@@ -10,7 +10,7 @@
 // @updateURL    https://gist.github.com/mchangrh/9507604353e37b6abc2f7f6b3c6e1338/raw/sbltnfi-imprecise-times.user.js
 // @downloadURL  https://gist.github.com/mchangrh/9507604353e37b6abc2f7f6b3c6e1338/raw/sbltnfi-imprecise-times.user.js
 // ==/UserScript==
- 
+
 // replace consequtive zeros at the end with nothing
 const reduceTime = time => time.replace(/(?<=\.\d+)0+$/g, '');
 
@@ -25,7 +25,8 @@ function findTimes() {
   if (startIndex === -1) return;
   table.querySelectorAll("tbody tr").forEach((row) => {
     for (const index of [startIndex, endIndex, lengthIndex]) {
-      const cell = row.children[index];
+      const parentCell = row.children[index]
+      const cell =  parentCell.querySelector("a") ?? parentCell;
       const oldValue = cell.innerText
       cell.innerText = reduceTime(oldValue);
     }
