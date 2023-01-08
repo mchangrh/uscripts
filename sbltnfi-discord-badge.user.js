0e3c466445caf7cedd9a2c6bb4c2fd5ee4187e07
Michael C <michael@mchang.name>
2023-01-08 15:09:53 -0500

    [badge] add support for all tables and all urls

--- sbltnfi-discord-badge.user.js
+++ sbltnfi-discord-badge.user.js
@@ -1,25 +1,26 @@
 // ==UserScript==
 // @name         sb.ltn.fi discord badge
 // @namespace    mchang.name
-// @version      1.0.4
+// @version      1.1.0
 // @description  Indicates if a SB user is on discord
 // @author       michael mchang.name
-// @match        https://sb.ltn.fi/userid/*
+// @match        https://sb.ltn.fi/*
 // @icon         https://cdn.mchang.xyz/uscript/discord-badge.png
 // @updateURL    https://gist.github.com/mchangrh/9507604353e37b6abc2f7f6b3c6e1338/raw/sbltnfi-discord-badge.user.js
 // @downloadURL  https://gist.github.com/mchangrh/9507604353e37b6abc2f7f6b3c6e1338/raw/sbltnfi-discord-badge.user.js
+// @connect      mongo.mchang.xyz
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 const AUTH = "tlB7XLNX3b33nbnx01hw"
 
-function lookupUser (SBID) {
+function lookupUser (SBID, target) {
   GM_xmlhttpRequest({
     method: "GET",
     url: `https://mongo.mchang.xyz/sb-vip/discord?auth=${AUTH}&sbID=${SBID}`,
     timeout: 1000,
-    onload: (res) => addBadge(res.status === 200),
-    onerror: (res) => addBadge(false)
+    onload: (res) => addBadge(res.status === 200, target),
+    onerror: (res) => addBadge(false, target)
   })
 }
 
@@ -32,15 +33,29 @@ spanElem.title = "This user is on Discord"
 spanElem.classList = "badge bg-secondary ms-1"
 spanElem.appendChild(discordBadge)
 
-function addBadge (onDiscord) {
-  // scope to header
-  const header = document.querySelector("div.row.mt-2 > .col-auto > .list-group")
-  const username = header.children[0]
-  if (onDiscord) username.appendChild(spanElem)
+function addBadge (onDiscord, target) {
+  if (!onDiscord) return
+  const clone = spanElem.cloneNode(true)
+  if (!target) {
+    // scope to header
+    const header = document.querySelector("div.row.mt-2 > .col-auto > .list-group")
+    const username = header.children[0]
+    username.appendChild(clone)
+  } else {
+    target.after(clone)
+  }
 }
 
 (function () {
   "use strict"
-  const SBID = new URL(document.URL).pathname.split("/")[2]
-  lookupUser(SBID)
+  const pathname = new URL(document.URL).pathname
+  if (pathname.includes("/userid/")) {
+    const SBID = pathname.split("/")[2]
+    lookupUser(SBID)
+  } else {
+    document.querySelectorAll("a[href^='/userid/']").forEach(elem => {
+      const SBID = elem.href.split("/")[4]
+      lookupUser(SBID, elem)
+    })
+  }
 })()
